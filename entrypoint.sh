180fd14b296bd37a281c051151582b6acccade80
Mark Wotton <mwotton@gmail.com>
2022-04-12 12:59:35 +0700

    kludge to change starting directory

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 set -euf
+# probably there is a nicer way to do this than forking it.
+cd monitor_project
 
 if [ -f ./requirements.txt ]; then
   safety check --bare --file ./requirements.txt
