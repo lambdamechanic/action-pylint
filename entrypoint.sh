8ef711bce8826c619ecfd0ed6ecbde702a0c3768
Mark Wotton <mwotton@gmail.com>
2022-04-12 15:42:52 +0700

    be noisier

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,11 @@ set -euf
 # probably there is a nicer way to do this than forking it.
 cd monitor_project
 
+echo "safety"
 if [ -f ./requirements.txt ]; then
   safety check --bare --file ./requirements.txt
 fi
-
+echo "black (formatting)"
 black --quiet --check .
+echo "flake8 (lint)"
 flake8 .
